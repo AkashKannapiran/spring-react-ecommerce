ef034f1d94a907b5f64447dfa79a78d38d618ce3
AkashKannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2026-09-07 19:35:13 +0530

    feat(backend): add Category model and REST controller
    with getAllCategories and  createCategories with List (not DB)

---
+++ backend/src/main/java/com/akash/backend/model/Category.java
@@ -0,0 +1,39 @@
+package com.akash.backend.model;
+
+public class Category {
+
+    private Long categoryID;
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(Long categoryID, String categoryName) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryID=" + categoryID +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+}
